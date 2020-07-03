7b03a6068ba018b1be93252de5dcb646784c70b6
DyfanJones <dyfan.jones@shopdirect.com>
2020-07-03 16:05:11 +0100

    added S3Downloader class

--- R/s3.R
+++ R/s3.R
@@ -2,10 +2,12 @@
 
 #' @include session.R
 #' @include utils.R
+#'
+#' @import R6
 
 # validation check of s3 uri
 is.s3_uri <- function(x) {
-  if(is.null(x)) return(FALSE)
+  if(is.null(x) || !is.character(x)) return(FALSE)
   regex <- '^s3://[a-z0-9][a-z0-9\\.-]+[a-z0-9](/(.*)?)?$'
   grepl(regex, x)
 }
@@ -40,7 +42,7 @@ S3Uploader = R6Class("S3Uploader",
                                          kms_key=NULL,
                                          sagemaker_session=NULL){
 
-                       sagemaker_session = session %||% Session$new()
+                       sagemaker_session = sagemaker_session %||% Session$new()
                        s3_parts = split_s3_uri(desired_s3_uri)
                        if (!is.null(kms_key))
                          return(sagemaker_session$upload_data(
@@ -52,6 +54,15 @@ S3Uploader = R6Class("S3Uploader",
                                   key_prefix=s3_parts$key))
                        },
 
+                       # Static method that uploads a given file or directory to S3.
+                       # Args:
+                       #   body (str): String representing the body of the file.
+                       # desired_s3_uri (str): The desired S3 uri to upload to.
+                       # kms_key (str): The KMS key to use to encrypt the files.
+                       # session (sagemaker.session.Session): AWS session to use. Automatically
+                       # generates one if not provided.
+                       # Returns:
+                       #   str: The S3 uri of the uploaded file(s).
                        upload_string_as_file_body = function(body,
                                                              desired_s3_uri=NULL,
                                                              kms_key=NULL,
@@ -64,7 +75,69 @@ S3Uploader = R6Class("S3Uploader",
                            key_prefix=s3_parts$key, kms_key=kms_key)
 
                          return(desired_s3_uri)
-
                        }
                      )
-                     )
+)
+
+
+S3Downloader = R6Class("S3Downloader",
+                       public =list(
+                         # Static method that downloads a given S3 uri to the local machine.
+                         # Args:
+                         #   s3_uri (str): An S3 uri to download from.
+                         # local_path (str): A local path to download the file(s) to.
+                         # kms_key (str): The KMS key to use to decrypt the files.
+                         # session (sagemaker.session.Session): Session object which
+                         # manages interactions with Amazon SageMaker APIs and any other
+                         # AWS services needed. If not specified, the estimator creates one
+                         # using the default AWS configuration chain.
+                         download = function(s3_uri,
+                                             local_path,
+                                             kms_key=NULL,
+                                             sagemaker_session=NULL){
+
+                           sagemaker_session = sagemaker_session %||% Session$new()
+                           s3_parts = split_s3_uri(desired_s3_uri)
+                           if (!is.null(kms_key))
+                             return(sagemaker_session$download_data(
+                               path=local_path, bucket=s3_parts$bucket,
+                               key_prefix=s3_parts$key, SSEKMSKeyId = kms_key))
+
+                           return(sagemaker_session$download_data(
+                             path=local_path, bucket=s3_parts$bucket,
+                             key_prefix=s3_parts$key))
+                         },
+
+                         # Static method that returns the contents of an s3 uri file body as a string.
+                         # Args:
+                         #   s3_uri (str): An S3 uri that refers to a single file.
+                         # session (sagemaker.session.Session): AWS session to use. Automatically
+                         # generates one if not provided.
+                         # Returns:
+                         #   str: The body of the file.
+                         read_file = function(s3_uri,
+                                              sagemaker_session=NULL){
+
+                           sagemaker_session = sagemaker_session %||% Session$new()
+                           s3_parts = split_s3_uri(desired_s3_uri)
+
+                          return(sagemaker_session$read_s3_file(bucket=s3_parts$bucket, key_prefix=s3_parts$key))
+                         },
+
+                         # Static method that lists the contents of an S3 uri.
+                         # Args:
+                         #   s3_uri (str): The S3 base uri to list objects in.
+                         # session (sagemaker.session.Session): AWS session to use. Automatically
+                         # generates one if not provided.
+                         # Returns:
+                         #   [str]: The list of S3 URIs in the given S3 base uri.
+                         list_s3_uri = function(s3_uri,
+                                                sagemaker_session = NULL){
+                           sagemaker_session = sagemaker_session %||% Session()
+                           s3_parts = split_s3_uri(desired_s3_uri)
+
+                           file_keys = sagemaker_session$list_s3_files(bucket=s3_parts$bucket, key_prefix=s3_parts$key)
+                           return(lapply(file_keys, function(file_key) sprintf("s3://%s/%s", s3_parts$bucket, file_key)))
+                         }
+                       )
+)
