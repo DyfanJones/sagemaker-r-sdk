1acd1c6deaff3e3728fb04817bcc1b7f2e97830c
DyfanJones <dyfan.jones@shopdirect.com>
2020-07-07 11:17:16 +0100

    added TrainingJobAnalytics class

--- R/analytics.R
+++ R/analytics.R
@@ -5,6 +5,7 @@
 
 #' @import logger
 #' @import R6
+#' @import paws
 #' @import data.table
 
 
@@ -45,7 +46,8 @@ AnalyticsMetricsBase = R6Class("AnalyticsMetricsBase",
                                  #'              time any properties are accessed they will be refreshed from the
                                  #'              service.
                                  clear_cache = function(){
-                                   return(self$.dataframe = NULL)
+                                   self$.dataframe = NULL
+                                   return(self$.dataframe)
                                  },
 
                                  #' @description
@@ -58,7 +60,7 @@ AnalyticsMetricsBase = R6Class("AnalyticsMetricsBase",
                                ),
                                private = list(
                                  # Sub-class must calculate the dataframe and return it.
-                                 .fetch_dataframe = function(){stop("I'm an abstract interface method", call. = F)},
+                                 .fetch_dataframe = function(){stop("I'm an abstract interface method", call. = F)}
                                ),
                                lock_objects = F
 )
@@ -86,6 +88,8 @@ HyperparameterTuningJobAnalytics = R6Class("HyperparameterTuningJobAnalytics",
                                                self$.training_job_summaries = NULL
                                                super$initialize()
                                                self$clear_cache()
+                                               attr(self, "__repr__") = sprintf("<HyperparameterTuningJobAnalytics for %s>", self$name)
+
                                              },
 
                                              #' @description Clear the object of all local caches of API methods.
@@ -192,10 +196,6 @@ HyperparameterTuningJobAnalytics = R6Class("HyperparameterTuningJobAnalytics",
                                              }
                                            ),
                                            private = list(
-                                             ..repr..=function(){
-                                                return (sprintf("<HyperparameterTuningJobAnalytics for %s>",self$name))
-                                             },
-
                                              .fetch_dataframe = function(){
                                                # Run that helper over all the summaries.
                                                df = rbindlist(lapply(training_summary, function(training_summary){
@@ -227,4 +227,136 @@ HyperparameterTuningJobAnalytics = R6Class("HyperparameterTuningJobAnalytics",
                                            ),
                                            lock_objects = F
 )
+
+
+#' @title TrainingJobAnalytics Class
+#' @description Fetch training curve data from CloudWatch Metrics for a specific training
+#'              job.
+#' @export
+TrainingJobAnalytics = R6Class("TrainingJobAnalytics",
+                               inherit = AnalyticsMetricsBase,
+                               public = list(
+                                 #' @field CLOUDWATCH_NAMESPACE
+                                 #' class metadata
+                                 CLOUDWATCH_NAMESPACE = "/aws/sagemaker/TrainingJobs",
+
+                                 #' @description Initialize a ``TrainingJobAnalytics`` instance.
+                                 #' @param training_job_name (str): name of the TrainingJob to analyze.
+                                 #' @param metric_names (list, optional): string names of all the metrics to
+                                 #'              collect for this training job. If not specified, then it will
+                                 #'              use all metric names configured for this job.
+                                 #' @param sagemaker_session (sagemaker.session.Session): Session object which
+                                 #'              manages interactions with Amazon SageMaker APIs and any other
+                                 #'              AWS services needed. If not specified, one is specified using
+                                 #'              the default AWS configuration chain.
+                                 #' @param start_time :
+                                 #' @param end_time :
+                                 #' @param period :
+                                 initialize = function(training_job_name,
+                                                       metric_names=NULL,
+                                                       sagemaker_session=NULL,
+                                                       start_time=NULL,
+                                                       end_time=NULL,
+                                                       period=NULL){
+                                   self$sagemaker_session = sagemaker_session %||% Session$new()
+                                   self$.cloudwatch = cloudwatch(config = self$sagemaker_session$paws_credentials$credentials)
+                                   self$.training_job_name = training_job_name
+                                   self$.start_time = start_time
+                                   self$.end_time = end_time
+                                   self$.period = period %||% METRICS_PERIOD_DEFAULT
+
+                                   if (!is.null(metric_names))
+                                     self$.metric_names = metric_names
+                                   else
+                                     self$.metric_names = private$.metric_names_for_training_job()
+
+                                   super$initialize()
+                                   self$clear_cache()
+
+                                   attr(self, "__repr__") = sprintf("<sagemaker.TrainingJobAnalytics for %s>", self$name)
+                                 },
+
+                                 #' @description Clear the object of all local caches of API methods, so that the next
+                                 #'              time any properties are accessed they will be refreshed from the
+                                 #'              service.
+                                 clear_cache = function(){
+                                   super$clear_cache()
+                                   self$.data = data.table()
+                                   self$.time_interval = private$.determine_timeinterval()
+                                 }
+                               ),
+                               private = list(
+                                 # Return a dictionary with two datetime objects, start_time and
+                                 # end_time, covering the interval of the training job
+                                 .determine_timeinterval=function(){
+                                   description = self$sagemaker_session$sagemaker$describe_training_job(TrainingJobName=self$name)
+                                   start_time = self$.start_time %||% description$TrainingStartTime  # datetime object
+                                   # Incrementing end time by 1 min since CloudWatch drops seconds before finding the logs.
+                                   # This results in logs being searched in the time range in which the correct log line was
+                                   # not present.
+                                   # Example - Log time - 2018-10-22 08:25:55
+                                   #       Here calculated end time would also be 2018-10-22 08:25:55 (without 1 min addition)
+                                   #       CW will consider end time as 2018-10-22 08:25 and will not be able to search the
+                                   #           correct log.
+                                   end_time = self$.end_time %||% description$TrainingEndTime + 60 # add 1 minute
+
+                                   return(list("start_time"= start_time, "end_time"= end_time))
+                                 },
+
+                                 .fetch_dataframe = function(){
+                                   self$.data= lapply(self$.metric_names, private$.fetch_metric)
+                                   return(rbindlist(self$.data, fill = T))
+                                 },
+
+                                 # Fetch all the values of a named metric, and add them to _data
+                                 # Args:
+                                 #   metric_name:
+                                 .fetch_metric = function(metric_name){
+                                   raw_cwm_data = self$.cloudwatch$get_metric_statistics(
+                                        Namespace= self$CLOUDWATCH_NAMESPACE,
+                                        MetricName= metric_name,
+                                        Dimensions= list(list("Name"= "TrainingJobName", "Value"= self$name)),
+                                        StartTime= self$.time_interval$start_time,
+                                        EndTime= self$.time_interval$end_time,
+                                        Period= self$.period,
+                                        Statistics= list("Average"))$Datapoints
+                                   if (islistempty(raw_cwm_data)){
+                                     log_warn("Warning: No metrics called %s found", metric_name)
+                                     return(NULL)}
+                                   # Process data: normalize to starting time, and sort.
+                                   base_time = min(raw_cwm_data$Timestamp)
+
+                                   all_xy= rbindlist(lapply(raw_cwm_data, function(pt)
+                                     data.table(timestamp = as.numeric(pt$Timestamp - base_time),
+                                                metric_name	= metric_name,
+                                                value = pt$Average)),
+                                     fill = T)
+
+                                   setorder(all_xy, Timestamp)
+                                   return(all_xy)
+                                 },
+
+                                 # Helper method to discover the metrics defined for a training job.
+                                 .metric_names_for_training_job = function(){
+                                   raining_description = self$sagemaker_session$sagemaker$describe_training_job(
+                                     TrainingJobName=self$.training_job_name)
+
+                                   metric_definitions = training_description$AlgorithmSpecification$MetricDefinitions
+                                   metric_names = lapply(metric_definitions, function(md) md$Name)
+
+                                   return(metric_names)
+                                 }
+
+                               ),
+                               active = list(
+
+                                 #' @field name
+                                 #' Name of the TrainingJob being analyzed
+                                 name = function(){
+                                  return(self$.training_job_name)
+                                 }
+                               ),
+                               lock_objects = F
+)
+
 
