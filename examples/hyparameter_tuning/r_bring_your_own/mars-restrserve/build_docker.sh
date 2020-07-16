95d05380ae939a1558f56f649406f0335a175fa6
DyfanJones <dyfan.jones@shopdirect.com>
2020-07-16 14:38:34 +0100

    added shell script to build and upload docker image to ecr

---
+++ examples/hyparameter_tuning/r_bring_your_own/mars-restrserve/build_docker.sh
@@ -0,0 +1,31 @@
+# modified from: https://github.com/awslabs/amazon-sagemaker-examples/blob/master/r_examples/r_byo_r_algo_hpo/tune_r_bring_your_own.ipynb
+
+# The name of our algorithm
+algorithm_name=rmars
+
+#set -e # stop if anything fails
+account=$(aws sts get-caller-identity --query Account --output text)
+
+# Get the region defined in the current configuration (default to us-west-2 if none defined)
+region=$(aws configure get region)
+region=${region:-us-east-1}
+
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
+
+# If the repository doesn't exist in ECR, create it.
+aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
+fi
+
+# Get the login command from ECR and execute it directly
+aws ecr get-login-password | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
+
+# Build the docker image locally with the image name and then push it to ECR
+# with the full name.
+docker build  -t ${algorithm_name} .
+docker tag ${algorithm_name} ${fullname}
+
+docker push ${fullname}
