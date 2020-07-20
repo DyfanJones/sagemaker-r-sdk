8fab439feb4778f91cfa4bd8cbee56267552703f
DyfanJones <dyfan.jones@shopdirect.com>
2020-07-20 10:23:53 +0100

    export S3Uploader/S3Downloader classes

--- R/s3.R
+++ R/s3.R
@@ -22,21 +22,22 @@ split_s3_uri <- function(uri) {
   )
 }
 
-
+#' @title S3Uploader Class
+#' @description Contains static methods for uploading directories or files to S3
+#' @export
 S3Uploader = R6Class("S3Uploader",
   public = list(
-   # Static method that uploads a given file or directory to S3.
-   # Args:
-   #   local_path (str): Path (absolute or relative) of local file or directory to upload.
-   # desired_s3_uri (str): The desired S3 location to upload to. It is the prefix to
-   # which the local filename will be added.
-   # kms_key (str): The KMS key to use to encrypt the files.
-   # session (sagemaker.session.Session): Session object which
-   # manages interactions with Amazon SageMaker APIs and any other
-   # AWS services needed. If not specified, the estimator creates one
-   # using the default AWS configuration chain.
-   # Returns:
-   #   The S3 uri of the uploaded file(s).
+
+   #' @description Static method that uploads a given file or directory to S3.
+   #' @param local_path (str): Path (absolute or relative) of local file or directory to upload.
+   #' @param desired_s3_uri (str): The desired S3 location to upload to. It is the prefix to
+   #'              which the local filename will be added.
+   #' @param kms_key (str): The KMS key to use to encrypt the files.
+   #' @param sagemaker_session (sagemaker.session.Session): Session object which
+   #'              manages interactions with Amazon SageMaker APIs and any other
+   #'              AWS services needed. If not specified, the estimator creates one
+   #'              using the default AWS configuration chain.
+   #' @return The S3 uri of the uploaded file(s).
    upload = function(local_path = NULL,
                      desired_s3_uri = NULL,
                      kms_key=NULL,
@@ -54,15 +55,13 @@ S3Uploader = R6Class("S3Uploader",
               key_prefix=s3_parts$key))
    },
 
-   # Static method that uploads a given file or directory to S3.
-   # Args:
-   #   body (str): String representing the body of the file.
-   # desired_s3_uri (str): The desired S3 uri to upload to.
-   # kms_key (str): The KMS key to use to encrypt the files.
-   # session (sagemaker.session.Session): AWS session to use. Automatically
-   # generates one if not provided.
-   # Returns:
-   #   str: The S3 uri of the uploaded file(s).
+   #' @description Static method that uploads a given file or directory to S3.
+   #' @param body (str): String representing the body of the file.
+   #' @param desired_s3_uri (str): The desired S3 uri to upload to.
+   #' @param kms_key (str): The KMS key to use to encrypt the files.
+   #' @param sagemaker_session (sagemaker.session.Session): AWS session to use. Automatically
+   #'              generates one if not provided.
+   #' @return str: The S3 uri of the uploaded file(s).
    upload_string_as_file_body = function(body,
                                          desired_s3_uri=NULL,
                                          kms_key=NULL,
@@ -79,18 +78,20 @@ S3Uploader = R6Class("S3Uploader",
   )
 )
 
-
+#' @title S3Downloader
+#' @description Contains static methods for downloading directories or files from S3.
+#' @export
 S3Downloader = R6Class("S3Downloader",
   public =list(
-   # Static method that downloads a given S3 uri to the local machine.
-   # Args:
-   #   s3_uri (str): An S3 uri to download from.
-   # local_path (str): A local path to download the file(s) to.
-   # kms_key (str): The KMS key to use to decrypt the files.
-   # session (sagemaker.session.Session): Session object which
-   # manages interactions with Amazon SageMaker APIs and any other
-   # AWS services needed. If not specified, the estimator creates one
-   # using the default AWS configuration chain.
+
+   #' @description Static method that downloads a given S3 uri to the local machine.
+   #' @param s3_uri (str): An S3 uri to download from.
+   #' @param local_path (str): A local path to download the file(s) to.
+   #' @param kms_key (str): The KMS key to use to decrypt the files.
+   #' @param sagemaker_session (sagemaker.session.Session): Session object which
+   #'              manages interactions with Amazon SageMaker APIs and any other
+   #'              AWS services needed. If not specified, the estimator creates one
+   #'              using the default AWS configuration chain.
    download = function(s3_uri,
                        local_path,
                        kms_key=NULL,
@@ -108,13 +109,11 @@ S3Downloader = R6Class("S3Downloader",
        key_prefix=s3_parts$key))
    },
 
-   # Static method that returns the contents of an s3 uri file body as a string.
-   # Args:
-   #   s3_uri (str): An S3 uri that refers to a single file.
-   # session (sagemaker.session.Session): AWS session to use. Automatically
-   # generates one if not provided.
-   # Returns:
-   #   str: The body of the file.
+   #' @description Static method that returns the contents of an s3 uri file body as a string.
+   #' @param s3_uri (str): An S3 uri that refers to a single file.
+   #' @param sagemaker_session (sagemaker.session.Session): AWS session to use. Automatically
+   #'              generates one if not provided.
+   #' @return str: The body of the file.
    read_file = function(s3_uri,
                         sagemaker_session=NULL){
 
@@ -124,13 +123,11 @@ S3Downloader = R6Class("S3Downloader",
     return(sagemaker_session$read_s3_file(bucket=s3_parts$bucket, key_prefix=s3_parts$key))
    },
 
-   # Static method that lists the contents of an S3 uri.
-   # Args:
-   #   s3_uri (str): The S3 base uri to list objects in.
-   # session (sagemaker.session.Session): AWS session to use. Automatically
-   # generates one if not provided.
-   # Returns:
-   #   [str]: The list of S3 URIs in the given S3 base uri.
+   #' @description Static method that lists the contents of an S3 uri.
+   #' @param s3_uri (str): The S3 base uri to list objects in.
+   #' @param sagemaker_session (sagemaker.session.Session): AWS session to use. Automatically
+   #'              generates one if not provided.
+   #' @return [str]: The list of S3 URIs in the given S3 base uri.
    list_s3_uri = function(s3_uri,
                           sagemaker_session = NULL){
      sagemaker_session = sagemaker_session %||% Session()
