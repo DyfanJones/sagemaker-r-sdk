62ad65126cfb714879a7d47c32c130d303e56423
DyfanJones <dyfan.jones@shopdirect.com>
2020-07-24 17:22:49 +0100

    converted python validation helper functions validation helper class

---
+++ R/amazon_validation.R
@@ -0,0 +1,31 @@
+# NOTE: This code has been modified from AWS Sagemaker Python: https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/amazon/validation.py
+
+#' @import R6
+
+Validation = R6Class("Validation",
+  public = list(
+    gt = function(minimum){
+      return(function(value){value > minimum})
+    },
+
+    ge = function(minimum){
+      return(function(value){value >= minimum})
+    },
+
+    lt = function(maximum){
+      return(function(value){value < maximum})
+    },
+
+    le = function(maximum){
+      return(function(value){value <= maximum})
+    },
+
+    isin = function(expected){
+      return(function(value){value %in% expected})
+    },
+
+    istype = function(expected){
+      return(function(value){inherits(valuem, expected)})
+    }
+  )
+)
