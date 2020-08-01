e86ec601e5d9a67b42b5c04537443a24489a692c
DyfanJones <dyfan.jones@shopdirect.com>
2020-08-01 16:17:25 +0100

    Initial commit of classes: matrix_to_record_serializer, record_deserializer. Helper functions to write matrices to protobuffer: .write_feature_tensor, .write_label_tensor, .write_keys_tensor, .write_shape, write_matrix_to_dense_tensor, write_spmatrix_to_sparse_tensor. Helper function to write proto message to binary file (with padding) .write_recordio. Helper function to convert R matrix types for protobuffer message: .resolve_type. Finally helper function to split matrix into shard when sending to protobuffer: .build_shards

---
+++ R/amazon_common.R
@@ -0,0 +1,205 @@
+
+#' @import R6
+
+matrix_to_record_serializer = R6Class("matrix_to_record_serializer",
+  public = list(
+    initialize = function(content_type="application/x-recordio-protobuf"){
+      self$content_type = content_type
+    },
+
+    serialize = function(array){
+      if(length(dim(array)) == 1)
+        # reshape array
+
+      obj = raw(0)
+      buf = rawConnection(obj, open = "wb")
+      write_matrix_to_dense_tensor(buf, array)
+      close(buf)
+
+      return(obj)
+    }
+  )
+)
+
+record_deserializer = R6Class("record_deserializer",
+  public = list(
+    initialize = function(accept="application/x-recordio-protobuf"){
+      self$accept = accept
+    },
+
+    deserializer = function(stream,
+                            content_type){
+
+      # TODO: create read_records function
+      tryCatch(read_records(stream),
+               finally = function(f) close(stream))
+    }
+  )
+)
+
+.write_feature_tensor <- function(resolved_type, record, vector){
+  if (resolved_type == "Int32")
+    record$features[[1]]$value$int32_tensor$values <- vector
+  if (resolved_type == "Float64")
+    record$features[[1]]$value$float64_tensor$values <- vector
+  if (resolved_type == "Float32")
+    record$features[[1]]$value$float32_tensor$values <- vector
+}
+
+.write_label_tensor <- function(resolved_type, record, scalar){
+  if (resolved_type == "Int32")
+    record$label[[1]]$values$int32_tensor$values <- c(scalar)
+  if (resolved_type == "Float64")
+    record$label[[1]]$values$float64_tensor$values <- c(scalar)
+  if (resolved_type == "Float32")
+    record$label[[1]]$values$float32_tensor$values <- c(scalar)
+}
+
+.write_keys_tensor <- function(resolved_type, record, vector){
+  if (resolved_type == "Int32")
+    record$features[[1]]$value$int32_tensor$keys <- vector
+  if (resolved_type == "Float64")
+    record$features[[1]]$value$float64_tensor$keys <- vector
+  if (resolved_type == "Float32")
+    record.features[[1]]$value$float32_tensor$keys <- vector
+}
+
+.write_shape <- function(resolved_type, record, scalar){
+  if(resolved_type == "Int32")
+    record$features[[1]]$value$int32_tensor$shape <- c(scalar)
+  if (resolved_type == "Float64")
+    record$features[[1]]$values$float64_tensor$shape <- c(scalar)
+  if (resolved_type == "Float32")
+    record$features[[1]]$value$float32_tensor$shape <- c(scalar)
+}
+
+write_matrix_to_dense_tensor <- function(file, array, labels = NULL){
+  # Validate shape of array and labels, resolve array and label types
+  if (!length(dim(mat)) ==2)
+    stop("Array must be a Matrix", call. = F)
+
+  if(!is.null(labels)){
+    if (!length(dim(labels)) == 1)
+      stop("Labels must be a Vector", call. = F)
+    if (!(dim(labels)[1] %in% dim(array)))
+        stop(sprintf("Label shape (%s) not compatible with array shape (%s)",
+                     paste(dim(labels), collapse = ", "),
+                     paste(dim(array), collapse = ", ")),
+             call. = F)
+    # As matrix/sparse consist of all elements being the same clase can check first element
+    resolved_label_type = .resolve_type(labels[1])
+  }
+  resolved_type = .resolve_type(array[1])
+  # Write each vector in array into a Record in the file object
+  for(index in 1:nrow(array)){
+    vector = array[i,]
+    record = Record$clone()
+    .write_feature_tensor(resolved_type, record, vector)
+    if (!is.null(labels))
+      .write_label_tensor(resolved_label_type, record, labels[index])
+
+    .write_recordio(file, record)
+  }
+}
+
+write_spmatrix_to_sparse_tensor <- function(file, array, labels=NULL){
+  if (!Matrix::is(array, "sparseMatrix"))
+    stop("Array must be sparse", call. = F)
+
+  # Validate shape of array and labels, resolve array and label types
+  if (!length(dim(array)) == 2)
+    stop("Array must be a Matrix", call.=F)
+  if (!is.null(labels)){
+    # TODO: need to double check this method works in R
+    if (length(dim(labels)) != 1)
+      stop("Labels must be a Vector", call. = F)
+    if (!(dim(labels)[1] %in% dim(array)))
+      stop(sprintf("Label shape (%s) not compatible with array shape (%s)",
+                   paste(dim(labels), collapse = ", "),
+                   paste(dim(array), collapse = ", ")),
+           call. = F)
+    resolved_label_type = .resolve_type(labels[1])
+  }
+  resolved_type = .resolve_type(labels[1])
+
+  # csr_array = array.tocsr()
+  array_dim = dim(array)
+
+
+  for (row_idx in 1:nrow(array)){
+    record = Record$clone()
+    row = array[row_idx,]
+
+    # Write values
+    .write_feature_tensor(resolved_type, record, row.data)
+
+    # Write keys
+    .write_keys_tensor(resolved_type, record, row.indices.astype(np.uint64))
+
+    # Write labels
+    if (!is.null(labels))
+      .write_label_tensor(resolved_label_type, record, labels[row_idx])
+
+    # Write shape
+    .write_shape(resolved_type, record, n_cols)
+
+    # TODO: create .write_recordio function
+    # TODO: replace record serializeToString with RProtobuf method
+    .write_recordio(file, record.SerializeToString())
+  }
+}
+
+# Eagerly read a collection of amazon Record protobuf objects from file
+read_records = function(file){
+  records = list
+  # TODO: read_recordio
+  for (record_data in read_recordio(file)){
+    record = Record()
+    record.ParseFromString(record_data)
+    records.append(record)
+  }
+  return(records)
+}
+
+# MXNet requires recordio records have length in bytes that's a multiple of 4
+# This sets up padding bytes to append to the end of the record, for diferent
+# amounts of padding required.
+padding = list()
+for (amount in 0:3){
+  padding[[amount+1]] = writeBin(rep(0x00, amount), raw())
+}
+
+.kmagic = 0xCED7230A
+
+.write_recordio = function(f, data){
+  len = length(record$serialize(NULL))
+  writeBin(.kmagic, f)
+  writeBin(len, f)
+  pad = 1 + bitwShiftL(bitwShiftR((len + 3), 2), 2) - len # added +1 to map to R indexing
+  serialize_pb(record, f)
+  writeBin(padding[[pad]], f)
+}
+
+.resolve_type=function(dtype){
+  switch(typeof(dtype),
+         integer =   "Int32",
+         integer64 = "Int32",
+         numeric =   "Float64",
+         double = "Float64",
+         stop(sprintf("Unsupported dtype %s on array",dtype), call. = FALSE)
+  )
+}
+
+.build_shards = function(num_shards, array){
+  if (num_shards < 1)
+    stop("num_shards must be >= 1")
+  # ensure matrix gets split into same number of shards
+  shard_size = ceiling(dim(array)[1] / num_shards)
+  if (shard_size == 0)
+    stop("Array length is less than num shards")
+
+  max_row = dim(array)[2]
+
+  split_vec <- seq(1, max_row, shard_size)
+  lapply(split_vec,function(i)array[i:min(max_row,(i+shard_size-1)),])
+}
