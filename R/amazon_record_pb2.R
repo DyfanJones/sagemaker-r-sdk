ead8e0cf0b8f34ba39071696bb050053af07a608
DyfanJones <dyfan.jones@shopdirect.com>
2020-08-03 10:50:13 +0100

    moved assign of internal record.proto file to onload

--- R/amazon_record_pb2.R
+++ R/amazon_record_pb2.R
@@ -1,8 +1,6 @@
+#' @importFrom RProtoBuf P
 
-#' @importFrom RProtobuf P
-
-# get record.proto
-record.proto = system.file("proto", "record.proto", package= "R6sagemaker")
+#' @include zzz.R
 
 # create descriptors
 .FLOAT32TENSOR <- RProtoBuf::P("aialgs.data.Float32Tensor", file = record.proto)
@@ -10,14 +8,14 @@ record.proto = system.file("proto", "record.proto", package= "R6sagemaker")
 .INT32TENSOR <- RProtoBuf::P("aialgs.data.Int32Tensor", file = record.proto)
 .BYTES <- RProtoBuf::P("aialgs.data.Bytes", file =record.proto)
 .VALUE <- RProtoBuf::P("aialgs.data.Value", file =record.proto)
-.RECORD_FEATURESENTRY <-RProtoBuf::P("aialgs.data.Record.FeaturesEntry", file = record.proto)
-.RECORD_LABELENTRY = RProtoBuf::P("aialgs.data.Record.LabelEntry", file = record.proto)
+.RECORD_FEATURESENTRY <- RProtoBuf::P("aialgs.data.Record.FeaturesEntry", file = record.proto)
+.RECORD_LABELENTRY <- RProtoBuf::P("aialgs.data.Record.LabelEntry", file = record.proto)
 .RECORD <- RProtoBuf::P("aialgs.data.Record", file = record.proto)
 
 # Create message
-Float32Tensor = .FLOAT32TENSOR$new()
-Float64Tensor = .FLOAT64TENSOR$new()
-Int32Tensor = .INT32TENSOR$new()
-Bytes = .BYTES$new()
-Value = .VALUE$new()
-Record = .RECORD$new(features = .RECORD_FEATURESENTRY$new(), label = .RECORD_LABELENTRY$new())
+Float32Tensor <- .FLOAT32TENSOR$new()
+Float64Tensor <- .FLOAT64TENSOR$new()
+Int32Tensor <- .INT32TENSOR$new()
+Bytes <- .BYTES$new()
+Value <- .VALUE$new()
+Record <- .RECORD$new(features = .RECORD_FEATURESENTRY$new(), label = .RECORD_LABELENTRY$new())
--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   sagemaker_logging_format()
+  record.proto <<- system.file("proto", "record.proto", package= "R6sagemaker", lib.loc=libname)
+  assign("record.proto",record.proto ,envir = parent.env(environment()))
 }
