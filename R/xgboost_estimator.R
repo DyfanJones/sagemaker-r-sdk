7cd160c14e5802336d109c76bc4e3e6f72c88050
DyfanJones <dyfan.jones@shopdirect.com>
2020-08-22 17:38:00 +0100

    deprecated get_xgboost_uri function

--- R/xgboost_estimator.R
+++ R/xgboost_estimator.R
@@ -1,17 +1,3 @@
 # NOTE: This code has been modified from AWS Sagemaker Python: https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/xgboost/estimator.py
 
-
-# get XGBoost framework image URI
-
 #' @include fw_registry.R
-
-#' @title Get XGBoost framework image URI
-#' @param region (str): The aws region in which docker image is stored.
-#' @param frame_version (str):
-#' @param py_version (str): python version
-#' @export
-get_xgboost_image_uri <- function(region, framework_version, py_version = "py3"){
-  image_tag <- sprintf("%s-%s-%s",framework_version, "cpu", py_version)
-  fw_default_framework_uri(XGBOOST_NAME, region, image_tag)
-}
-
