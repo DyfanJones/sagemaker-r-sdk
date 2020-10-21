e3940dcedfd2b2c0daf791fdf850ab69cf42276a
DyfanJones <dyfan.jones@shopdirect.com>
2020-10-21 16:58:43 +0100

    added side_effect method and add_function to Mock class. New helper test function call_args to return arguments

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -2,7 +2,7 @@
 # used for testing only
 Mock <- R6::R6Class("Mock",
   public = list(
-    initialize = function(name, ... ){
+    initialize = function(name, ...){
       if(!missing(name)) class(self) <- append(name, class(self))
       args = list(...)
       for(arg in names(args)){
@@ -10,14 +10,33 @@ Mock <- R6::R6Class("Mock",
       }
     },
     return_value = function(value){
-      self$value = value
+      if(is.function(value))
+        stop("`value` is a function, please use `side_effect`.", call. = FALSE)
+      private$.value = value
       return(private$.return)
+    },
+
+    side_effect = function(effect){
+      private$.effect = effect
+      return(private$.effect)
+    },
+
+    add_function = function(name, meth) {
+      self[[name]] <- meth
+      environment(self[[name]]) <- environment(self$add_function)
     }
   ),
   private = list(
+    .value = NULL,
+    .effect = NULL,
     .return = function(...){
-      return(self$value)
+      return(private$.value)
     }
   ),
   lock_objects = F
 )
+
+call_args = function(...){
+  private$.args = list(...)
+  return(private$.args)
+}
