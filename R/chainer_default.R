6c39e913a96358e8cf58609ac08bd0284fe8ae3c
DyfanJones <dyfan.jones@shopdirect.com>
2020-10-25 19:20:22 +0000

    chainer default variables

---
+++ R/chainer_default.R
@@ -0,0 +1,3 @@
+# NOTE: This code has been modified from AWS Sagemaker Python: https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/chainer/defaults.py
+
+LATEST_PY2_VERSION = "5.0.0"
