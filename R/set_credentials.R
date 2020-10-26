5e78891fb98267c3d28cfa7062403ba59c87462e
DyfanJones <dyfan.jones@shopdirect.com>
2020-10-26 19:17:36 +0000

    code formatting

--- R/set_credentials.R
+++ R/set_credentials.R
@@ -56,8 +56,15 @@ PawsCredentials = R6Class("PawsCredentials",
       self$aws_secret_access_key = aws_secret_access_key %||% get_aws_env("AWS_SECRET_ACCESS_KEY")
       self$aws_session_token = aws_session_token %||% get_aws_env("AWS_SESSION_TOKEN")
       self$region_name = region_name %||% get_region(profile_name)
-      self$profile_name = if(!(is.null(self$aws_access_key_id) || is.null(self$aws_secret_access_key) || is.null(self$aws_session_token))) NULL else get_profile_name(profile_name)
-      self$credentials = private$cred_set(self$aws_access_key_id, self$aws_secret_access_key, self$aws_session_token, self$profile_name, self$region_name)
+      self$profile_name = if(!(is.null(self$aws_access_key_id) ||
+                               is.null(self$aws_secret_access_key) ||
+                               is.null(self$aws_session_token))) NULL else get_profile_name(profile_name)
+      self$credentials = private$cred_set(
+        self$aws_access_key_id,
+        self$aws_secret_access_key,
+        self$aws_session_token,
+        self$profile_name,
+        self$region_name)
     },
 
     #' @description
