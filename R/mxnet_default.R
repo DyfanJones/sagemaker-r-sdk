46ee92e867b1a98b9b0a76215b2ea6f20df90819
DyfanJones <dyfan.jones@shopdirect.com>
2020-11-03 12:48:41 +0000

    initial mxnet estimator, model, predictor classes

---
+++ R/mxnet_default.R
@@ -0,0 +1,3 @@
+# NOTE: This code has been modified from AWS Sagemaker Python: https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/mxnet/defaults.py
+
+MXNET_LATEST_PY2_VERSION = "1.6.0"
