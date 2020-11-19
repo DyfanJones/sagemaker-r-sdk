90a18f54102684bd205de1fb84e592fc1d715536
DyfanJones <dyfan.jones@shopdirect.com>
2020-11-19 16:58:38 +0000

    deprecation functions to inform users R6sagemaker will be aligned to python sagemaker v2

--- R/deprecations.R
+++ R/deprecations.R
@@ -2,14 +2,58 @@
 
 #' @import logger
 
-V2_URL <- "https://sagemaker.readthedocs.io/en/stable/v2.html"
+V2_URL = "https://sagemaker.readthedocs.io/en/stable/v2.html"
 
-# Generic warning raiser referencing V2
-# Args:
-#   phrase: The phrase to include in the warning.
 .warn <- function(msg){
-  full_msg = sprintf("%s in python sagemaker>=2.\nSee: %s for details.", msg, V2_URL)
+  full_msg = sprintf("%s to align with python sagemaker>=2.\nSee: %s for details.", msg, V2_URL)
+  warning(full_msg, call. = F)
   log_warn(full_msg)
+}
+
+# Raise a warning for a no-op in sagemaker>=2
+# Args:
+#   phrase: the prefix phrase of the warning message.
+removed_warning <- function(phrase){
+  .warn(sprintf("%s is a no-op", phrase))
+}
+
+# Raise a warning for a rename in sagemaker>=2
+# Args:
+#   phrase: the prefix phrase of the warning message.
+renamed_warning <- function(phrase){
+  .warn(sprintf("%s has been renamed", phrase))
+}
+
+# Checks if the deprecated argument is in kwargs
+# Raises warning, if present.
+# Args:
+#   old_name: name of deprecated argument
+# new_name: name of the new argument
+# value: value associated with new name, if supplied
+# kwargs: keyword arguments dict
+# Returns:
+#   value of the keyword argument, if present
+renamed_kwargs <- function(old_name,
+                           new_name,
+                           value,
+                           kwargs){
+  if(old_name %in% names(renamed_kwargs)){
+    value = kwargs[[old_name]] %||% value
+    eval.parent(substitute({kwargs[[new_name]] = value}))
+    renamed_warning(old_name)}
+  return(value)
+}
+
+# Checks if the deprecated argument is populated.
+# Raises warning, if not None.
+# Args:
+#   name: name of deprecated argument
+# arg: the argument to check
+remove_arg <- function(name,
+                       arg = NULL){
+  if(!is.null(arg)){
+    removed_warning(name)
+  }
 }
 
 # Checks if the deprecated argument is in kwargs
@@ -17,10 +61,8 @@ V2_URL <- "https://sagemaker.readthedocs.io/en/stable/v2.html"
 # Args:
 #   name: name of deprecated argument
 # kwargs: keyword arguments dict
-removed_kwargs <- function(name, kwargs){
-  msg = "%s is a no-op"
+removed_kwargs <- function(name,
+                           kwargs){
   if (name %in% names(kwargs))
-    .warn(sprintf(msg, name))
+    removed_warning(name)
 }
-
-
