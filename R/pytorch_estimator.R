a3773cc3312d741e7ac83b455ee56844cd65006a
DyfanJones <dyfan.jones@shopdirect.com>
2020-11-21 19:46:14 +0000

    initial pytorch classes: estimator, model and predictor

--- R/pytorch_estimator.R
+++ R/pytorch_estimator.R
@@ -1,48 +1,44 @@
-# NOTE: This code has been modified from AWS Sagemaker Python: https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/mxnet/estimator.py
+# NOTE: This code has been modified from AWS Sagemaker Python: https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/pytorch/estimator.py
 
-#' @include deprecations.R
 #' @include estimator.R
 #' @include fw_utils.R
-#' @include mxnet_default.R
-#' @include mxnet_model.R
+#' @include pytorch_defaults.R
+#' @include pytorch_model.R
 #' @include vpc_utils.R
+#' @include utils.R
 
 #' @import R6
 #' @import logger
 
-#' @title MXNet Class
-#' @description Handle end-to-end training and deployment of custom MXNet code.
+#' @title PyTorch Class
+#' @description Handle end-to-end training and deployment of custom PyTorch code.
 #' @export
-MXNet = R6Class("MXNet",
+PyTorch = R6Class("PyTorch",
   inherit = Framework,
   public = list(
 
-    #' @field .LOWEST_SCRIPT_MODE_VERSION
-    #' Lowest MXNet version that can be executed
-    .LOWEST_SCRIPT_MODE_VERSION = "1.3",
-
-    #' @description This ``Estimator`` executes an MXNet script in a managed MXNet
+    #' @description This ``Estimator`` executes an PyTorch script in a managed PyTorch
     #'              execution environment, within a SageMaker Training Job. The managed
-    #'              MXNet environment is an Amazon-built Docker container that executes
+    #'              PyTorch environment is an Amazon-built Docker container that executes
     #'              functions defined in the supplied ``entry_point`` Python script.
     #'              Training is started by calling
     #'              :meth:`~sagemaker.amazon.estimator.Framework.fit` on this Estimator.
     #'              After training is complete, calling
     #'              :meth:`~sagemaker.amazon.estimator.Framework.deploy` creates a hosted
     #'              SageMaker endpoint and returns an
-    #'              :class:`~sagemaker.amazon.mxnet.model.MXNetPredictor` instance that can
-    #'              be used to perform inference against the hosted model.
-    #'              Technical documentation on preparing MXNet scripts for SageMaker
-    #'              training and using the MXNet Estimator is available on the project
+    #'              :class:`~sagemaker.amazon.pytorch.model.PyTorchPredictor` instance that
+    #'              can be used to perform inference against the hosted model.
+    #'              Technical documentation on preparing PyTorch scripts for SageMaker
+    #'              training and using the PyTorch Estimator is available on the project
     #'              home-page: https://github.com/aws/sagemaker-python-sdk
     #' @param entry_point (str): Path (absolute or relative) to the Python source
     #'              file which should be executed as the entry point to training.
     #'              If ``source_dir`` is specified, then ``entry_point``
     #'              must point to a file located at the root of ``source_dir``.
-    #' @param framework_version (str): MXNet version you want to use for executing
-    #'              your model training code. Defaults to `None`. Required unless
-    #'              ``image_uri`` is provided. List of supported versions.
-    #'              https://github.com/aws/sagemaker-python-sdk#mxnet-sagemaker-estimators.
+    #' @param framework_version (str): PyTorch version you want to use for
+    #'              executing your model training code. Defaults to ``None``. Required unless
+    #'              ``image_uri`` is provided. List of supported versions:
+    #'              https://github.com/aws/sagemaker-python-sdk#pytorch-sagemaker-estimators.
     #' @param py_version (str): Python version you want to use for executing your
     #'              model training code. One of 'py2' or 'py3'. Defaults to ``None``. Required
     #'              unless ``image_uri`` is provided.
@@ -57,42 +53,36 @@ MXNet = R6Class("MXNet",
     #'              SageMaker. For convenience, this accepts other types for keys
     #'              and values, but ``str()`` will be called to convert them before
     #'              training.
-    #' @param image_uri (str): If specified, the estimator will use this image for training and
-    #'              hosting, instead of selecting the appropriate SageMaker official image based on
-    #'              framework_version and py_version. It can be an ECR url or dockerhub image and tag.
+    #' @param image_uri (str): If specified, the estimator will use this image
+    #'              for training and hosting, instead of selecting the appropriate
+    #'              SageMaker official image based on framework_version and
+    #'              py_version. It can be an ECR url or dockerhub image and tag.
     #'              Examples:
     #'              * ``123412341234.dkr.ecr.us-west-2.amazonaws.com/my-custom-image:1.0``
     #'              * ``custom-image:latest``
     #'              If ``framework_version`` or ``py_version`` are ``None``, then
     #'              ``image_uri`` is required. If also ``None``, then a ``ValueError``
     #'              will be raised.
-    #' @param distribution (dict): A dictionary with information on how to run distributed
-    #'              training (default: None). Currently we support distributed training with
-    #'              parameter server and MPI [Horovod].
-    #' @param  ... : Additional kwargs passed to the
-    #'              :class:`~sagemaker.estimator.Framework` constructor.
+    #' @param ... : Additional kwargs passed to the :class:`~sagemaker.estimator.Framework`
+    #'              constructor.
     initialize = function(entry_point,
                           framework_version=NULL,
                           py_version=NULL,
                           source_dir=NULL,
                           hyperparameters=NULL,
                           image_uri=NULL,
-                          distribution=NULL,
                           ...){
       kwargs = list(...)
-      distribution = renamed_kwargs("distributions", "distribution", distribution, kwargs)
-      instance_type = renamed_kwargs(
-        "train_instance_type", "instance_type", kwargs$instance_type, kwargs
-      )
+
       validate_version_or_image_args(framework_version, py_version, image_uri)
 
       self$framework_version = framework_version
       self$py_version = py_version
 
-      if (!("enable_sagemaker_metrics" %in% names(kwargs))){
-        # enable sagemaker metrics for MXNet v1.6 or greater:
-        if (!is.null(self$framework_version) && package_version(self$framework_version) >= package_version("1.6"))
-        kwargs$enable_sagemaker_metrics = TRUE
+      if (!islistempty(kwargs$enable_sagemaker_metrics)){
+        # enable sagemaker metrics for PT v1.3 or greater:
+        if (self$framework_version && (package_version(self$framework_version) >= package_version("1.3")))
+          kwargs$enable_sagemaker_metrics = TRUE
       }
 
       kwargs = c(entry_point = entry_point,
@@ -103,22 +93,15 @@ MXNet = R6Class("MXNet",
 
       do.call(super$initialize, kwargs)
 
-      attr(self, "_framework_name") = "mxnet"
+      attr(self, "_framework_name") = "pytorch"
 
       if (identical(py_version, "py2"))
         log_warn(
-          python_deprecation_warning(attr(self, "_framework_name"), MXNET_LATEST_PY2_VERSION)
+          python_deprecation_warning(attr(self, "_framework_name"), PYTORCH_LATEST_PY2_VERSION)
         )
-
-      if (!is.null(distribution))
-        warn_if_parameter_server_with_multi_gpu(
-          training_instance_type=instance_type, distribution=distribution
-        )
-
-      private$.configure_distribution(distribution)
     },
 
-    #' @description Create a SageMaker ``MXNetModel`` object that can be deployed to an
+    #' @description Create a SageMaker ``PyTorchModel`` object that can be deployed to an
     #'              ``Endpoint``.
     #' @param model_server_workers (int): Optional. The number of worker processes
     #'              used by the inference server. If None, server will use one
@@ -141,82 +124,41 @@ MXNet = R6Class("MXNet",
     #'              any additional libraries that will be exported to the container.
     #'              If not specified, the dependencies from training are used.
     #'              This is not supported with "local code" in Local Mode.
-    #' @param image_uri (str): If specified, the estimator will use this image for hosting, instead
-    #'              of selecting the appropriate SageMaker official image based on framework_version
-    #'              and py_version. It can be an ECR url or dockerhub image and tag.
-    #'              Examples:
-    #'              * ``123412341234.dkr.ecr.us-west-2.amazonaws.com/my-custom-image:1.0``
-    #'              * ``custom-image:latest``
-    #' @param ... : Additional kwargs passed to the :class:`~sagemaker.mxnet.model.MXNetModel`
+    #' @param ... : Additional kwargs passed to the :class:`~sagemaker.pytorch.model.PyTorchModel`
     #'              constructor.
-    #' @return sagemaker.mxnet.model.MXNetModel: A SageMaker ``MXNetModel`` object.
-    #'              See :func:`~sagemaker.mxnet.model.MXNetModel` for full details.
+    #' @return sagemaker.pytorch.model.PyTorchModel: A SageMaker ``PyTorchModel``
+    #'              object. See :func:`~sagemaker.pytorch.model.PyTorchModel` for full details.
     create_model = function(model_server_workers=NULL,
                             role=NULL,
                             vpc_config_override="VPC_CONFIG_DEFAULT",
                             entry_point=NULL,
                             source_dir=NULL,
                             dependencies=NULL,
-                            image_uri=NULL,
                             ...){
       kwargs = list(...)
       if (!("image_uri" %in% names(kwargs)))
-        kwargs$image_uri = image_uri %||% self$image_uri
-
+        kwargs$image_uri = self$image_uri
       kwargs$name = private$.get_or_create_name(kwargs$name)
 
       kwargs = c(list(
-        model_data = self$model_data,
-        role = role %||% self$role,
-        entry_point = entry_point,
+        model_data=self$model_data,
+        role=role %||% self$role,
+        entry_point=entry_point %||% private$.model_entry_point(),
         framework_version=self$framework_version,
         py_version=self$py_version,
-        source_dir=source_dir %||% private$.model_source_dir(),
+        source_dir=(source_dir %||% self._model_source_dir()),
         container_log_level=self$container_log_level,
         code_location=self$code_location,
         model_server_workers=model_server_workers,
         sagemaker_session=self$sagemaker_session,
         vpc_config=self$get_vpc_config(vpc_config_override),
-        dependencies=dependencies %||% self$dependencies),
-        kwargs
-      )
+        dependencies=(dependencies %||% self$dependencies)),
+        kwargs)
 
-      model = do.call(MXNetModel$new, kwargs)
-
-      if (is.null(entry_point))
-        model$entry_point = (
-          if (model$.__enclos_env__$private$.is_mms_version())
-            self$entry_point
-          else private$.model_entry_point()
-        )
-
-      return(model)
+      return (do.call(PyTorchModel$new, kwargs))
     }
   ),
   private = list(
-    .configure_distribution = function(distribution){
-      if (is.null(distribution))
-        return(invisible(NULL))
-
-      if (!is.null(self$framework_version) &&
-          package_version(self$framework_version) < package_version(self$.LOWEST_SCRIPT_MODE_VERSION))
-        stop(sprintf("The distribution option is valid for only versions %s and higher",
-                     self$.LOWEST_SCRIPT_MODE_VERSION), call. = F)
-
-      enabled = distribution$parameter_server$enabled %||% FALSE
-      self$.hyperparameters[[self$LAUNCH_PS_ENV_NAME]] = enabled
-
-      if(islistempty(distribution$mpi)){
-        mpi_dict = distribution$mpi
-        mpi_enabled = mpi_dict$enabled %||% FALSE
-        self$.hyperparameters[[self$LAUNCH_MPI_ENV_NAME]] = mpi_enabled
-
-        if(!islistempty(mpi_dict$processes_per_host)){
-          self$.hyperparameters[[self$MPI_NUM_PROCESSES_PER_HOST]] = mpi_dict$processes_per_host
-          self$.hyperparameters[[self$MPI_CUSTOM_MPI_OPTIONS]] = mpi_dict$custom_mpi_options %||% ""
-        }
-      }
-    },
 
     # Convert the job description to init params that can be handled by the
     # class constructor
@@ -230,36 +172,31 @@ MXNet = R6Class("MXNet",
     .prepare_init_params_from_job_description = function(job_details,
                                                          model_channel_name=NULL){
       init_params = super$.prepare_init_params_from_job_description(
-        job_details, model_channel_name
-      )
+        job_details, model_channel_name)
+
       image_uri = init_params$image_uri
       init_params$image_uri = NULL
       img_split = framework_name_from_image(image_uri)
       names(img_split) = c("framework", "py_version", "tag", "scriptmode")
 
-      # We switched image tagging scheme from regular image version (e.g. '1.0') to more
-      # expressive containing framework version, device type and python version
-      # (e.g. '0.12-gpu-py2'). For backward compatibility map deprecated image tag '1.0' to a
-      # '0.12' framework version otherwise extract framework version from the tag itself.
       if (is.null(img_split$tag)) {
         framework_version = NULL
-      } else if (img_split$tag == "1.0") {
-        framework_version = "0.12"
       } else {
         framework_version = framework_version_from_tag(img_split$tag)}
       init_params$framework_version = framework_version
       init_params$py_version = img_split$py_version
 
-      if (is.null(img_split$framework)) {
+      if (img_split$framework){
         # If we were unable to parse the framework name from the image it is not one of our
         # officially supported images, in this case just add the image to the init params.
         init_params$image_uri = image_uri
-        return(init_params)}
+        return(init_params)
+      }
 
       if (img_split$framework != attr(self, "_framework_name"))
         stop(sprintf(
           "Training job: %s didn't use image for requested framework",
-            job_details$TrainingJobName),
+          job_details$TrainingJobName),
           call. = F)
 
       return(init_params)
---
+++ R/pytorch_model.R
@@ -0,0 +1,183 @@
+# NOTE: This code has been modified from AWS Sagemaker Python: https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/pytorch/model.py
+
+#' @include session.R
+#' @include image_uris.R
+#' @include deserializers.R
+#' @include fw_utils.R
+#' @include model.R
+#' @include pytorch_defaults.R
+#' @include pytorch_model.R
+#' @include predictor.R
+#' @include serializers.R
+#' @include utils.R
+
+#' @import R6
+#' @import logger
+
+#' @title A Predictor for inference against PyTorch Endpoints.
+#' @description This is able to serialize Python lists, dictionaries, and numpy arrays to
+#'              multidimensional tensors for PyTorch inference.
+#' @export
+PyTorchPredictor = R6Class("PyTorchPredictor",
+  inherit = Predictor,
+  public = list(
+
+    #' @description Initialize an ``PyTorchPredictor``.
+    #' @param endpoint_name (str): The name of the endpoint to perform inference
+    #'              on.
+    #' @param sagemaker_session (sagemaker.session.Session): Session object which
+    #'              manages interactions with Amazon SageMaker APIs and any other
+    #'              AWS services needed. If not specified, the estimator creates one
+    #'              using the default AWS configuration chain.
+    initialize = function(endpoint_name,
+                          sagemaker_session=NULL){
+      super$initialize(
+        endpoint_name, sagemaker_session, NumpySerializer$new(), NumpyDeserializer$new()
+      )
+    }
+  ),
+  lock_objects = F
+)
+
+#' @title PyTorchModel class
+#' @description An PyTorch SageMaker ``Model`` that can be deployed to a SageMaker
+#'              ``Endpoint``.
+#' @export
+PyTorchModel = R6Class("PyTorchModel",
+  inherit = FrameworkModel,
+  public = list(
+
+    #' @field .LOWEST_MMS_VERSION
+    #' Lowest Multi Model Server PyTorch version that can be executed
+    .LOWEST_MMS_VERSION = "1.2",
+
+    #' @description Initialize a PyTorchModel.
+    #' @param model_data (str): The S3 location of a SageMaker model data
+    #'              ``.tar.gz`` file.
+    #' @param role (str): An AWS IAM role (either name or full ARN). The Amazon
+    #'              SageMaker training jobs and APIs that create Amazon SageMaker
+    #'              endpoints use this role to access training data and model
+    #'              artifacts. After the endpoint is created, the inference code
+    #'              might use the IAM role, if it needs to access an AWS resource.
+    #' @param entry_point (str): Path (absolute or relative) to the Python source
+    #'              file which should be executed as the entry point to model
+    #'              hosting. If ``source_dir`` is specified, then ``entry_point``
+    #'              must point to a file located at the root of ``source_dir``.
+    #' @param framework_version (str): PyTorch version you want to use for
+    #'              executing your model training code. Defaults to None. Required
+    #'              unless ``image_uri`` is provided.
+    #' @param py_version (str): Python version you want to use for executing your
+    #'              model training code. Defaults to ``None``. Required unless
+    #'              ``image_uri`` is provided.
+    #' @param image_uri (str): A Docker image URI (default: None). If not specified, a
+    #'              default image for PyTorch will be used. If ``framework_version``
+    #'              or ``py_version`` are ``None``, then ``image_uri`` is required. If
+    #'              also ``None``, then a ``ValueError`` will be raised.
+    #' @param predictor_cls (callable[str, sagemaker.session.Session]): A function
+    #'              to call to create a predictor with an endpoint name and
+    #'              SageMaker ``Session``. If specified, ``deploy()`` returns the
+    #'              result of invoking this function on the created endpoint name.
+    #' @param model_server_workers (int): Optional. The number of worker processes
+    #'              used by the inference server. If None, server will use one
+    #'              worker per vCPU.
+    #' @param ... : Keyword arguments passed to the superclass
+    #'              :class:`~sagemaker.model.FrameworkModel` and, subsequently, its
+    #'              superclass :class:`~sagemaker.model.Model`.
+    initialize = function(model_data,
+                          role,
+                          entry_point,
+                          framework_version=NULL,
+                          py_version=NULL,
+                          image_uri=NULL,
+                          predictor_cls=PyTorchPredictor,
+                          model_server_workers=NULL,
+                          ...){
+      validate_version_or_image_args(framework_version, py_version, image_uri)
+
+      self$framework_version = framework_version
+      self$py_version = py_version
+
+      super$initialize(
+        model_data, image_uri, role, entry_point, predictor_cls=predictor_cls, ...)
+
+      attr(self, "_framework_name") = "pytorch"
+
+      if (identical(py_version, "py2"))
+        log_warn(
+          python_deprecation_warning(attr(self, "_framework_name"), PYTORCH_LATEST_PY2_VERSION)
+        )
+
+      self$model_server_workers = model_server_workers
+    },
+
+    #' @description Return a container definition with framework configuration set in
+    #'              model environment variables.
+    #' @param instance_type (str): The EC2 instance type to deploy this Model to.
+    #'              For example, 'ml.p2.xlarge'.
+    #' @param accelerator_type (str): The Elastic Inference accelerator type to
+    #'              deploy to the instance for loading and making inferences to the
+    #'              model.
+    #' @return dict[str, str]: A container definition object usable with the
+    #'              CreateModel API.
+    prepare_container_def = function(instance_type=NULL,
+                                     accelerator_type=NULL){
+      deploy_image = self$image_uri
+      if (is.null(deploy_image)){
+        if (is.null(instance_type))
+          stop("Must supply either an instance type (for choosing CPU vs GPU) or an image URI.",
+               call. = F)
+      }
+
+      region_name = self$sagemaker_session$paws_region_name
+      deploy_image = self$serving_image_uri(
+        region_name, instance_type, accelerator_type=accelerator_type
+      )
+
+      deploy_key_prefix = model_code_key_prefix(self$key_prefix, self$name, deploy_image)
+      private$.upload_code(deploy_key_prefix, repack=private$.is_mms_version())
+      deploy_env = list(self$env)
+      deploy_env = c(deploy_env, private$.framework_env_vars())
+
+      if (!islistempty(self$model_server_workers))
+        deploy_env[[toupper(MODEL_SERVER_WORKERS_PARAM_NAME)]] = as.character(self$model_server_workers)
+      return(sagemaker.container_def(
+        deploy_image, self$repacked_model_data %||% self$model_data, deploy_env)
+      )
+    },
+
+    #' @description Create a URI for the serving image.
+    #' @param region_name (str): AWS region where the image is uploaded.
+    #' @param instance_type (str): SageMaker instance type. Used to determine device type
+    #'              (cpu/gpu/family-specific optimized).
+    #' @param accelerator_type (str): The Elastic Inference accelerator type to
+    #'              deploy to the instance for loading and making inferences to the
+    #'              model.
+    #' @return str: The appropriate image URI based on the given parameters
+    serving_image_uri = function(region_name,
+                                 instance_type,
+                                 accelerator_type=NULL){
+      return (ImageUris$new()$retrieve(
+        attr(self, "_framework_name"),
+        region_name,
+        version=self$framework_version,
+        py_version=self$py_version,
+        instance_type=instance_type,
+        accelerator_type=accelerator_type,
+        image_scope="inference")
+      )
+    }
+  ),
+  private = list(
+
+    # Whether the framework version corresponds to an inference image using
+    # the Multi-Model Server (https://github.com/awslabs/multi-model-server).
+    # Returns:
+    #   bool: If the framework version corresponds to an image using MMS.
+    .is_mms_version = function(){
+      lowest_mms_version = package_version(self$.LOWEST_MMS_VERSION)
+      framework_version = package_version(self$framework_version)
+      return (framework_version >= lowest_mms_version)
+    }
+  ),
+  lock_objects = F
+)
