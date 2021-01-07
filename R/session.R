1a3d7945e073dcbc55e51c0d296a5677b40697ca
DyfanJones <dyfan.jones@shopdirect.com>
2021-01-07 14:25:39 +0000

    feature: split parameter creation of create_training_job into separate private method

--- R/session.R
+++ R/session.R
@@ -4,12 +4,29 @@
 #' @include logs.R
 #' @include set_credentials.R
 #' @include vpc_utils.R
+#' @include studio.R
 
 #' @import paws
 #' @import jsonlite
 #' @import R6
 #' @import logger
 #' @import utils
+
+NOTEBOOK_METADATA_FILE <- "/opt/ml/metadata/resource-metadata.json"
+
+.STATUS_CODE_TABLE <- list(
+  "COMPLETED"= "Completed",
+  "INPROGRESS"= "InProgress",
+  "FAILED"= "Failed",
+  "STOPPED"= "Stopped",
+  "STOPPING"= "Stopping",
+  "STARTING"= "Starting")
+
+LogState <- list(STARTING = 1,
+                 WAIT_IN_PROGRESS = 2,
+                 TAILING = 3,
+                 JOB_COMPLETE = 4,
+                 COMPLETE = 5)
 
 #' @title Sagemaker Session Class
 #'
@@ -286,7 +303,9 @@ Session = R6Class("Session",
     #'              Series. For more information see:
     #'              https://docs.aws.amazon.com/sagemaker/latest/dg/API_AlgorithmSpecification.html#SageMaker-Type-AlgorithmSpecification-EnableSageMakerMetricsTimeSeries
     #'              (Default: \code{NULL}).
-    #'
+    #' @param profiler_rule_configs (list[dict]): A list of profiler rule configurations.
+    #' @param profiler_config (dict): Configuration for how profiling information is emitted
+    #'              with SageMaker Profiler. (default: ``None``).
     #' @return
     #' str: ARN of the training job, if it is created.
     train = function(input_mode,
@@ -311,55 +330,43 @@ Session = R6Class("Session",
                      debugger_rule_configs=NULL,
                      debugger_hook_config=NULL,
                      tensorboard_output_config=NULL,
-                     enable_sagemaker_metrics=NULL){
-      train_request = list(
-        AlgorithmSpecification = list(TrainingInputMode = input_mode),
-        OutputDataConfig = output_config,
-        TrainingJobName = job_name,
-        StoppingCondition = stop_condition,
-        ResourceConfig = resource_config,
-        RoleArn = role)
+                     enable_sagemaker_metrics=NULL,
+                     profiler_rule_configs=NULL,
+                     profiler_config=NULL){
 
-      if(!is.null(image_uri) && !is.null(algorithm_arn)) {
-        stop("image_uri and algorithm_arn are mutually exclusive.",
-             sprintf("Both were provided: image_uri: %s algorithm_arn: %s",image_uri, algorithm_arn), call. = F)}
-
-      if(is.null(image_uri) && is.null(algorithm_arn)){
-        stop("either image_uri or algorithm_arn is required. None was provided.", call. = F)}
-
-      train_request$InputDataConfig = input_config
-
-      train_request$AlgorithmSpecification$TrainingImage = image_uri
-      train_request$AlgorithmSpecification$AlgorithmName = algorithm_arn
-      train_request$AlgorithmSpecification$MetricDefinitions = metric_definitions
-      train_request$AlgorithmSpecification$EnableSageMakerMetricsTimeSeries = enable_sagemaker_metrics
-
-      train_request$HyperParameters = hyperparameters
-      train_request$Tags = tags
-      train_request$VpcConfig = vpc_config
-      train_request$ExperimentConfig = experiment_config
-      train_request$EnableNetworkIsolation = enable_network_isolation
-
-      train_request$EnableInterContainerTrafficEncryption = encrypt_inter_container_traffic
-      train_request$EnableManagedSpotTraining = use_spot_instances
-
-      train_request$CheckpointConfig = NULL
-
-      if (!is.null(checkpoint_s3_uri) || !is.null(checkpoint_local_path)) {
-        checkpoint_config = list()
-        checkpoint_config["S3Uri"] = checkpoint_s3_uri
-        checkpoint_config["LocalPath"] = checkpoint_local_path
-        train_request$CheckpointConfig = list(checkpoint_config)
-      }
-
-      train_request$DebugRuleConfigurations = debugger_rule_configs
-      train_request$DebugHookConfig = debugger_hook_config
-
-      train_request$TensorBoardOutputConfig = tensorboard_output_config
+      tags = .append_project_tags(tags)
+      train_request = private$.get_train_request(
+        input_mode=input_mode,
+        input_config=input_config,
+        role=role,
+        job_name=job_name,
+        output_config=output_config,
+        resource_config=resource_config,
+        vpc_config=vpc_config,
+        hyperparameters=hyperparameters,
+        stop_condition=stop_condition,
+        tags=tags,
+        metric_definitions=metric_definitions,
+        enable_network_isolation=enable_network_isolation,
+        image_uri=image_uri,
+        algorithm_arn=algorithm_arn,
+        encrypt_inter_container_traffic=encrypt_inter_container_traffic,
+        use_spot_instances=use_spot_instances,
+        checkpoint_s3_uri=checkpoint_s3_uri,
+        checkpoint_local_path=checkpoint_local_path,
+        experiment_config=experiment_config,
+        debugger_rule_configs=debugger_rule_configs,
+        debugger_hook_config=debugger_hook_config,
+        tensorboard_output_config=tensorboard_output_config,
+        enable_sagemaker_metrics=enable_sagemaker_metrics,
+        profiler_rule_configs=profiler_rule_configs,
+        profiler_config=profiler_config)
 
       log_info("Creating training-job with name: %s", job_name)
       log_debug("train request: %s", toJSON(train_request, pretty = T, auto_unbox = T))
 
+      # TODO paws sdk is out of sync with boto3  i.e. [ProfilerRuleConfigurations, ProfilerConfig] parameters don't exist in `create_training_job` function.
+      # raise a ticket with paws to update package.
       self$sagemaker$create_training_job(TrainingJobName = train_request$TrainingJobName,
                              HyperParameters = train_request$HyperParameters,
                              AlgorithmSpecification = train_request$AlgorithmSpecification,
@@ -378,6 +385,8 @@ Session = R6Class("Session",
                              DebugRuleConfigurations = train_request$DebugRuleConfigurations,
                              TensorBoardOutputConfig = train_request$TensorBoardOutputConfig,
                              ExperimentConfig = train_request$ExperimentConfig)
+                             # ProfilerRuleConfigurations = train_request$ProfilerRuleConfigurations,
+                             # ProfilerConfig = train_request$ProfilerConfig)
     },
 
     #' @description Create an Amazon SageMaker processing job.
@@ -2292,6 +2301,141 @@ Session = R6Class("Session",
       return(tuning_config)
     },
 
+    # Constructs a request compatible for creating an Amazon SageMaker training job.
+    # Args:
+    #   input_mode (str): The input mode that the algorithm supports. Valid modes:
+    #   * 'File' - Amazon SageMaker copies the training dataset from the S3 location to
+    # a directory in the Docker container.
+    # * 'Pipe' - Amazon SageMaker streams data directly from S3 to the container via a
+    # Unix-named pipe.
+    # input_config (list): A list of Channel objects. Each channel is a named input source.
+    # Please refer to the format details described:
+    #   https://botocore.readthedocs.io/en/latest/reference/services/sagemaker.html#SageMaker.Client.create_training_job
+    # role (str): An AWS IAM role (either name or full ARN). The Amazon SageMaker training
+    # jobs and APIs that create Amazon SageMaker endpoints use this role to access
+    # training data and model artifacts. You must grant sufficient permissions to this
+    # role.
+    # job_name (str): Name of the training job being created.
+    # output_config (dict): The S3 URI where you want to store the training results and
+    # optional KMS key ID.
+    # resource_config (dict): Contains values for ResourceConfig:
+    #   * instance_count (int): Number of EC2 instances to use for training.
+    # The key in resource_config is 'InstanceCount'.
+    # * instance_type (str): Type of EC2 instance to use for training, for example,
+    # 'ml.c4.xlarge'. The key in resource_config is 'InstanceType'.
+    # vpc_config (dict): Contains values for VpcConfig:
+    #   * subnets (list[str]): List of subnet ids.
+    # The key in vpc_config is 'Subnets'.
+    # * security_group_ids (list[str]): List of security group ids.
+    # The key in vpc_config is 'SecurityGroupIds'.
+    # hyperparameters (dict): Hyperparameters for model training. The hyperparameters are
+    # made accessible as a dict[str, str] to the training code on SageMaker. For
+    # convenience, this accepts other types for keys and values, but ``str()`` will be
+    # called to convert them before training.
+    # stop_condition (dict): Defines when training shall finish. Contains entries that can
+    # be understood by the service like ``MaxRuntimeInSeconds``.
+    # tags (list[dict]): List of tags for labeling a training job. For more, see
+    # https://docs.aws.amazon.com/sagemaker/latest/dg/API_Tag.html.
+    # metric_definitions (list[dict]): A list of dictionaries that defines the metric(s)
+    # used to evaluate the training jobs. Each dictionary contains two keys: 'Name' for
+    # the name of the metric, and 'Regex' for the regular expression used to extract the
+    # metric from the logs.
+    # enable_network_isolation (bool): Whether to request for the training job to run with
+    # network isolation or not.
+    # image_uri (str): Docker image containing training code.
+    # algorithm_arn (str): Algorithm Arn from Marketplace.
+    # encrypt_inter_container_traffic (bool): Specifies whether traffic between training
+    # containers is encrypted for the training job (default: ``False``).
+    # use_spot_instances (bool): whether to use spot instances for training.
+    # checkpoint_s3_uri (str): The S3 URI in which to persist checkpoints
+    # that the algorithm persists (if any) during training. (default:
+    #                                                          ``None``).
+    # checkpoint_local_path (str): The local path that the algorithm
+    # writes its checkpoints to. SageMaker will persist all files
+    # under this path to `checkpoint_s3_uri` continually during
+    # training. On job startup the reverse happens - data from the
+    # s3 location is downloaded to this path before the algorithm is
+    # started. If the path is unset then SageMaker assumes the
+    # checkpoints will be provided under `/opt/ml/checkpoints/`.
+    # (default: ``None``).
+    # experiment_config (dict): Experiment management configuration. Dictionary contains
+    # three optional keys, 'ExperimentName', 'TrialName', and 'TrialComponentDisplayName'.
+    # (default: ``None``)
+    # enable_sagemaker_metrics (bool): enable SageMaker Metrics Time
+    # Series. For more information see:
+    #   https://docs.aws.amazon.com/sagemaker/latest/dg/API_AlgorithmSpecification.html#SageMaker-Type-AlgorithmSpecification-EnableSageMakerMetricsTimeSeries
+    # (default: ``None``).
+    # profiler_rule_configs (list[dict]): A list of profiler rule configurations.
+    # profiler_config(dict): Configuration for how profiling information is emitted with
+    # SageMaker Profiler. (default: ``None``).
+    # Returns:
+    #   Dict: a training request dict
+    .get_train_args = function(input_mode,
+                               input_config,
+                               role,
+                               job_name,
+                               output_config,
+                               resource_config,
+                               vpc_config,
+                               hyperparameters,
+                               stop_condition,
+                               tags,
+                               metric_definitions,
+                               enable_network_isolation=FALSE,
+                               image_uri=NULL,
+                               algorithm_arn=NULL,
+                               encrypt_inter_container_traffic=FALSE,
+                               use_spot_instances=FALSE,
+                               checkpoint_s3_uri=NULL,
+                               checkpoint_local_path=NULL,
+                               experiment_config=NULL,
+                               debugger_rule_configs=NULL,
+                               debugger_hook_config=NULL,
+                               tensorboard_output_config=NULL,
+                               enable_sagemaker_metrics=NULL,
+                               profiler_rule_configs=NULL,
+                               profiler_config=NULL){
+      train_request = list(
+        "AlgorithmSpecification"=list("TrainingInputMode"= input_mode),
+        "OutputDataConfig"= output_config,
+        "TrainingJobName"= job_name,
+        "StoppingCondition"= stop_condition,
+        "ResourceConfig"= resource_config,
+        "RoleArn"= role)
+
+      if (!is.null(image_uri) && !is.null(algorithm_arn))
+        stop("image_uri and algorithm_arn are mutually exclusive.",
+             sprintf("Both were provided: image_uri: %s algorithm_arn: %s", image_uri, algorithm_arn),
+             call. = F)
+      if (is.null(image_uri) && is.null(algorithm_arn))
+        stop("either image_uri or algorithm_arn is required. None was provided.", call. = F)
+
+      train_request$AlgorithmSpecification$TrainingImage = image_uri
+      train_request$AlgorithmSpecification$AlgorithmName = algorithm_arn
+      train_request$InputDataConfig = input_config
+      train_request$AlgorithmSpecification$MetricDefinitions = metric_definitions
+      train_request$AlgorithmSpecification$EnableSageMakerMetricsTimeSeries = enable_sagemaker_metrics
+      train_request$HyperParameters = hyperparameters
+      train_request$Tags = tags
+      train_request$VpcConfig = vpc_config
+      if (!islistempty(experiment_config))
+        train_request$ExperimentConfig = experiment_config
+      train_request$EnableNetworkIsolation = enable_network_isolation
+      train_request$EnableInterContainerTrafficEncryption = encrypt_inter_container_traffic
+      train_request$EnableManagedSpotTraining = use_spot_instances
+      if (!is.null(checkpoint_s3_uri)){
+        checkpoint_config = list("S3Uri"= checkpoint_s3_uri)
+        checkpoint_config["LocalPath"] = checkpoint_local_path
+        train_request$CheckpointConfig = checkpoint_config
+      }
+      train_request$DebugRuleConfigurations = debugger_rule_configs
+      train_request$DebugHookConfig = debugger_hook_config
+      train_request$TensorBoardOutputConfig = tensorboard_output_config
+      train_request$ProfilerRuleConfigurations = profiler_rule_configs
+      train_request$ProfilerConfig = profiler_config
+      return(train_request)
+    },
+
     .map_training_config = function(static_hyperparameters,
                                     input_mode,
                                     role,
@@ -2869,18 +3013,3 @@ get_execution_role <- function(sagemaker_session = NULL){
   }
 }
 
-LogState <- list(STARTING = 1,
-                WAIT_IN_PROGRESS = 2,
-                TAILING = 3,
-                JOB_COMPLETE = 4,
-                COMPLETE = 5)
-
-.STATUS_CODE_TABLE <- list(
-  "COMPLETED"= "Completed",
-  "INPROGRESS"= "InProgress",
-  "FAILED"= "Failed",
-  "STOPPED"= "Stopped",
-  "STOPPING"= "Stopping",
-  "STARTING"= "Starting")
-
-NOTEBOOK_METADATA_FILE <- "/opt/ml/metadata/resource-metadata.json"
