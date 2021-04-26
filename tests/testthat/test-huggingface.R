65f7356d04ac288877e7c2847db039f63f08f202
DyfanJones <dyfan.jones@shopdirect.com>
2021-04-26 19:25:11 +0100

    unit test: added model creation check

--- tests/testthat/test-huggingface.R
+++ tests/testthat/test-huggingface.R
@@ -16,7 +16,7 @@ INSTANCE_COUNT = 1
 INSTANCE_TYPE = "ml.p2.xlarge"
 ACCELERATOR_TYPE = "ml.eia.medium"
 IMAGE_URI = "huggingface"
-JOB_NAME = sprintf("%s-%s", IMAGE_URI, TIMESTAMP)
+JOB_NAME = sprintf("%s-.*", IMAGE_URI)
 ROLE = "Dummy"
 REGION = "us-east-1"
 GPU = "ml.p2.xlarge"
@@ -74,53 +74,51 @@ sagemaker_session$call_args("compile_model")
   )
 }
 
-.create_train_job = function(version, base_framework_version){
+.create_train_job = function(version, base_framework_version, s3_inputs){
   return(list(
     "image_uri"=.get_full_gpu_image_uri(version, base_framework_version),
     "input_mode"="File",
     "input_config"= list(
       list(
-        "ChannelName"="training",
         "DataSource"=list(
           "S3DataSource"=list(
-            "S3DataDistributionType"="FullyReplicated",
-            "S3DataType"="S3Prefix")
-        )
+            "S3DataType"="S3Prefix",
+            "S3Uri" = s3_inputs,
+            "S3DataDistributionType"="FullyReplicated"
+            )
+        ),
+        "ChannelName"="training"
       )
     ),
     "role"=ROLE,
     "job_name"=JOB_NAME,
     "output_config"=list("S3OutputPath"=sprintf("s3://%s/",BUCKET_NAME)),
     "resource_config"=list(
-      "InstanceType"=GPU,
       "InstanceCount"=1,
+      "InstanceType"=GPU,
       "VolumeSizeInGB"=30),
     "hyperparameters"=list(
       "sagemaker_program"="dummy_script.py",
-      "sagemaker_container_log_level"="INFO",
+      "sagemaker_container_log_level"="20",
       "sagemaker_job_name"=JOB_NAME,
       "sagemaker_submit_directory"=sprintf(
         "s3://%s/%s/source/sourcedir.tar.gz", BUCKET_NAME, JOB_NAME),
       "sagemaker_region"='us-east-1'),
     "stop_condition"=list("MaxRuntimeInSeconds"=24 * 60 * 60),
-    "tags"=NULL,
     "vpc_config"=NULL,
-    "metric_definitions"=NULL,
-    "environment"=NULL,
-    "experiment_config"=NULL,
     "debugger_hook_config"=list(
-      "CollectionConfigurations"=list(),
-      "S3OutputPath"=sprintf("s3://%s/",BUCKET_NAME)
+      "S3OutputPath"=sprintf("s3://%s/",BUCKET_NAME),
+      "CollectionConfigurations"=list()
       ),
     "profiler_rule_configs"=list(
       list(
-        "RuleConfigurationName"="ProfilerReport-1510006209",
+        "RuleConfigurationName"="ProfilerReport-.*",
         "RuleEvaluatorImage"="503895931360.dkr.ecr.us-east-1.amazonaws.com/sagemaker-debugger-rules:latest",
         "RuleParameters"=list("rule_to_invoke"="ProfilerReport")
       )
     ),
     "profiler_config"=list(
-      "S3OutputPath"= sprintf("s3://%s/"=BUCKET_NAME)
+      "S3OutputPath"= sprintf("s3://%s/",BUCKET_NAME)
       )
     )
   )
@@ -187,4 +185,90 @@ test_that("test huggingface invalid args", {
   )
 })
 
+test_that("test huggingface",{
+  huggingface_training_version="4.4"
+  huggingface_pytorch_version="1.6"
+  hf = HuggingFace$new(
+    py_version="py36",
+    entry_point=SCRIPT_PATH,
+    role=ROLE,
+    sagemaker_session=sagemaker_session,
+    instance_count=INSTANCE_COUNT,
+    instance_type=INSTANCE_TYPE,
+    transformers_version=huggingface_training_version,
+    pytorch_version=huggingface_pytorch_version,
+    enable_sagemaker_metrics=FALSE
+  )
+
+  inputs = "s3://mybucket/train"
+
+  hf$fit(inputs=inputs, experiment_config=EXPERIMENT_CONFIG)
+
+  expected_train_args = .create_train_job(
+    huggingface_training_version, sprintf("pytorch%s",huggingface_pytorch_version),inputs
+  )
+  expected_train_args[["experiment_config"]] = EXPERIMENT_CONFIG
+  expected_train_args[["enable_sagemaker_metrics"]] = FALSE
+  actual_train_args = sagemaker_session$train()$.call_args
+
+  expect_identical(sort(names(expected_train_args)), sort(names(actual_train_args)))
+
+  exp_hp = expected_train_args[["hyperparameters"]]
+  act_hp = actual_train_args[["hyperparameters"]]
+  expected_train_args[["hyperparameters"]] = NULL
+  actual_train_args[["hyperparameters"]] = NULL
+  for(i in names(exp_hp)){
+    expect_true(grepl(exp_hp[[i]], act_hp[[i]]))
+  }
+
+  exp_RuleConfigurationName = expected_train_args[["profiler_rule_configs"]][[1]][["RuleConfigurationName"]]
+  act_RuleConfigurationName = actual_train_args[["profiler_rule_configs"]][[1]][["RuleConfigurationName"]]
+
+  expected_train_args[["profiler_rule_configs"]][[1]][["RuleConfigurationName"]] = NULL
+  actual_train_args[["profiler_rule_configs"]][[1]][["RuleConfigurationName"]] = NULL
+
+  expect_true(grepl(exp_RuleConfigurationName, act_RuleConfigurationName))
+
+  for (i in l_name){
+    if(i != "job_name") {
+      expect_identical(expected_train_args[[i]], actual_train_args[[i]])
+    } else {
+      expect_true(grepl(expected_train_args[[i]], actual_train_args[[i]]))
+    }
+  }
+})
+
+# test_that("test attach", {
+#   training_image = sprintf(paste0(
+#     "1.dkr.ecr.us-east-1.amazonaws.com/huggingface-pytorch-training:%s-",
+#     f"transformers{huggingface_training_version}-gpu-py36-cu110-ubuntu18.04"
+#   )
+#   returned_job_description = {
+#     "AlgorithmSpecification": {"TrainingInputMode": "File", "TrainingImage": training_image},
+#     "HyperParameters": {
+#       "sagemaker_submit_directory": '"s3://some/sourcedir.tar.gz"',
+#       "sagemaker_program": '"iris-dnn-classifier.py"',
+#       "sagemaker_s3_uri_training": '"sagemaker-3/integ-test-data/tf_iris"',
+#       "sagemaker_container_log_level": '"logging.INFO"',
+#       "sagemaker_job_name": '"neo"',
+#       "training_steps": "100",
+#       "sagemaker_region": '"us-east-1"',
+#     },
+#     "RoleArn": "arn:aws:iam::366:role/SageMakerRole",
+#     "ResourceConfig": {
+#       "VolumeSizeInGB": 30,
+#       "InstanceCount": 1,
+#       "InstanceType": "ml.c4.xlarge",
+#     },
+#     "StoppingCondition": {"MaxRuntimeInSeconds": 24 * 60 * 60},
+#     "TrainingJobName": "neo",
+#     "TrainingJobStatus": "Completed",
+#     "TrainingJobArn": "arn:aws:sagemaker:us-west-2:336:training-job/neo",
+#     "OutputDataConfig": {"KmsKeyId": "", "S3OutputPath": "s3://place/output/neo"},
+#     "TrainingJobOutput": {"S3TrainingJobOutput": "s3://here/output.tar.gz"},
+#   }
+#   sagemaker_session.sagemaker_client.describe_training_job = Mock(
+#     name="describe_training_job", return_value=returned_job_description
+#   )
+# })
 
